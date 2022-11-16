bb660c56bdf863bbe3fd5d3a4145fcade4dc098f
andrematheus2 <am_arao@hotmail.com>
2022-11-16 09:25:14 -0300

    removendo duplicação com herança

--- modelo.py
+++ modelo.py
@@ -1,57 +1,47 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
+class Programa:
+    def __init__(self, nome, ano):
         self.__nome = nome.title()
+        self.ano = ano
+
+    @property
+    def like(self):
+        return self._like
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novoNome):
+        self._nome = novoNome
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def like(self):
-        return self.__like
-
-    def dar_like(self):
-        self.likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novoNome):
-        self.__nome = novoNome
-
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def like(self):
-        return self.__like
-
-    def dar_like(self):
-        self.likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novoNome):
-        self.__nome = novoNome
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+f'- Duração: {vingadores.duracao} - Likes: {vingadores._likes}')
 
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta._likes}')
