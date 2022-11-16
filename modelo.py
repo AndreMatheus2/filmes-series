6383980b74dfa82ed29f30b5a209ed58171a7a4e
andrematheus2 <am_arao@hotmail.com>
2022-11-16 12:26:51 -0300

    fugindo da complexidade

--- modelo.py
+++ modelo.py
@@ -1,14 +1,14 @@
 class Programa:
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self._likes = 0
 
     @property
     def like(self):
-        return self._like
+        return self._likes
 
-    def dar_like(self):
+    def dar_likes(self):
         self._likes += 1
 
     @property
@@ -19,47 +19,54 @@ class Programa:
     def nome(self, novoNome):
         self._nome = novoNome
 
-    def  __str__imprime(self):
-        return f'{self._nome} - {self.ano} - {self._like}'
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def  __str__(self):
-        return f'{self._nome} - {self.ano} - {self.duracao} - {self._like}'
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self._likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
     def __str__(self):
-         return f'{self._nome} - {self.ano} - {self.temporadas} - {self._like}'
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self._likes}'
 
-class PlayList(list):
+class PlayList:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+    @property
+    def listagem(self):
+        return self._programas
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-tmep = Filme('Todo mundo em pânico', 1999, 100)
-demolidor = Serie('Demolidor', 2016, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
-vingadores.dar_like()
-tmep.dar_like()
-tmep.dar_like()
-tmep.dar_like()
-tmep.dar_like()
-demolidor.dar_like()
-demolidor.dar_like()
-atlanta.dar_like()
-atlanta.dar_like()
-atlanta.dar_like()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-filmes_e_series = [vingadores, atlanta, demolidor, tmep]
-playlist_fim_de_semana = PlayList('fim de semana', filmes_e_series)
+listinha = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = PlayList('fim de semana', listinha)
 
-for programa in playlist_fim_de_semana:
+for programa in minha_playlist.listagem:
     print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
