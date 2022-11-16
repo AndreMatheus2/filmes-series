265102aacbbed171bea9f306edfafeddc7b785c4
andrematheus2 <am_arao@hotmail.com>
2022-11-16 11:17:11 -0300

    criando PlayList

--- modelo.py
+++ modelo.py
@@ -19,30 +19,49 @@ class Programa:
     def nome(self, novoNome):
         self._nome = novoNome
 
+    def  __str__imprime(self):
+        return f'{self._nome} - {self.ano} - {self._like}'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-     def imprime(self):
-         print(f'{self._nome} - {self.ano} - {self.duracao} - {self._like}')
+    def  __str__imprime(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} - {self._like}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    def __str__imprime(self):
+         return f'{self._nome} - {self.ano} - {self.temporadas} - {self._like}'
 
-     def imprime(self):
-         print(f'{self._nome} - {self.ano} - {self.temporadas} - {self._like}')
+class PlayList:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+    def tamanho(self):
+        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmeSerie = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = PlayList('fim de semana', filmes_e_series)
 
-for programa in filmeSerie:
-    programa.imprime
+for programa in filmes_e_series:
+    print(programa)
