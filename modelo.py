9b8943979900c14713c9b8327c892d24333e46ed
andrematheus2 <am_arao@hotmail.com>
2022-11-16 09:32:28 -0300

    reutilizando mais

--- modelo.py
+++ modelo.py
@@ -2,6 +2,7 @@ class Programa:
     def __init__(self, nome, ano):
         self.__nome = nome.title()
         self.ano = ano
+        self._likes = 0
 
     @property
     def like(self):
@@ -20,18 +21,13 @@ class Programa:
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self._likes = 0
-
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
