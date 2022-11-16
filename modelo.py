5f10165ff1e2930e02f25280b5b7e6b5bb1fcfd9
andrematheus2 <am_arao@hotmail.com>
2022-11-16 09:04:35 -0300

    encapsulando comportamento

--- modelo.py
+++ modelo.py
@@ -1,22 +1,57 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def like(self):
+        return self.__like
+
+    def dar_like(self):
+        self.likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def like(self):
+        return self.__like
+
+    def dar_like(self):
+        self.likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-f'- Duração: {vingadores.duracao}')
+f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
 
 atlanta = Serie('atlanta', 2018, 2)
-
+atlanta.dar_like()
+atlanta.dar_like()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-f'- Temporadas: {atlanta.temporadas}')
+f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
